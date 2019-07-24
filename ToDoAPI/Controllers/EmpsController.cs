b6c9920f22bd9f2ad2a0d47cddda5d6dfb0f4277
rjpd@netzero.com <rogerjd>
2019-07-24 19:22:18 -0400

    notes

--- ToDoAPI/Controllers/EmpsController.cs
+++ ToDoAPI/Controllers/EmpsController.cs
@@ -7,9 +7,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ToDoAPI.Models;
 
-Web API handles serialization for you, so you do not need to call JsonConvert.SerializeObject.That is why you are getting an escaped string as your output value.Just pass the datatable directly to CreateResponse.Web API will turn it into JSON or XML for you depending on what was sent in the Accept header of the request. (It uses Json.Net under the covers.)
+https://stackoverflow.com/questions/38865498/how-to-convert-datatable-to-generic-list-in-c-sharp
 
-return Request.CreateResponse(HttpStatusCode.OK, dt);
+https://itnext.io/adding-features-to-a-simple-blazor-mvvm-client-with-composition-f31bfb01e20a
+
+https://visualstudiomagazine.com/blogs/tool-tracker/2018/05/converting-datatables-to-json.aspx
+
+//Web API handles serialization for you, so you do not need to call JsonConvert.SerializeObject.That is why you are getting an escaped string as your output value.Just pass the datatable directly to CreateResponse.Web API will turn it into JSON or XML for you depending on what was sent in the Accept header of the request. (It uses Json.Net under the covers.)
+//return Request.CreateResponse(HttpStatusCode.OK, dt);
 
 namespace ToDoAPI.Controllers
 {
@@ -17,25 +22,24 @@ namespace ToDoAPI.Controllers
     [ApiController]
     public class EmpsController : ControllerBase
     {
-        private readonly TodoContext _context;
-        private DataTable<ToDoItem> tbl;
+        //private readonly TodoContext _context;
+        private DataTable tbl;
 
         public EmpsController(TodoContext context)
         {
-            _context = context;
+            tbl = new DataTable();
+            tbl.Columns.Add("num");
+            tbl.Columns.Add("name");
 
-            if (_context.ToDoItems.Count() == 0)
-            {
-                _context.ToDoItems.Add(new ToDoItem { Name = "Item1" });
-                _context.SaveChanges();
-            }
+            tbl.Rows.Add(1, "abc");
+            tbl.Rows.Add(2, "def");
+            tbl.Rows.Add(3, "ghi");
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ToDoItem>>> GetTodoItems()
+        public async DataTable GetTodoItems()
         {
-            return await _context.ToDoItems.ToListAsync();
+            return tbl;
         }
-
     }
 }
